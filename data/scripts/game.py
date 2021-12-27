eec0d1c4a60b67c2f273d7b50a61661caa98a4a6
Marko Lialiuk <49954445+Robomarchello@users.noreply.github.com>
2021-12-27 15:30:33 +0200

    Better targets

--- data/scripts/game.py
+++ data/scripts/game.py
@@ -60,14 +60,22 @@ class Armored_Target():
     def __init__(self,pos,images):
         self.crntImg = 0
         self.images = images
-        self.rect = self.image.get_rect(center=pos)
+        self.destroyed = False
+        self.rect = self.images[0].get_rect(center=pos)
 
         self.speed = 2
         self.speedmult = 0.7
 
     def draw(self,screen):
         self.rect.y += self.speed*self.speedmult
-        screen.blit(self.image,self.rect.topleft)
+        
+        if self.crntImg == len(self.images)-1:
+            self.destroyed = True
+            
+            
+        screen.blit(self.images[self.crntImg],self.rect.topleft)
+
+
 
 class Showing_Target():
     def __init__(self,pos1,pos2,image,destImg,brick,duration):
@@ -132,12 +140,28 @@ class Showing_Target():
         self.imgrect.center = [self.brickRect.right,self.imgrect.center[1]]
         self.imgrect.center = [self.imgrect.center[0],self.brickRect.center[1]]
 
+class target_piece():
+    def __init__(self,pos,image):
+        self.pos = pos
+        self.pieceImg = image
+        self.pieceImg.set_colorkey((0,0,0))
 
+        self.speed = rd.uniform(0.2,0.7)*rd.choice([-1,1]) 
+        self.angle = 0
+
+    def draw(self,screen):
+        self.angle += self.speed
+
+        self.pos[1] += 2
+        rotImg = pygame.transform.rotate(self.pieceImg,self.angle)
+        screen.blit(rotImg,self.pos)
         
 class Target_System():
     def __init__(self,cursor):
         self.score = 0
         self.wave = 0
+
+        self.pieces = []
 
         self.cursor = cursor
 
@@ -154,9 +178,28 @@ class Target_System():
         self.showTargetDestroyed.set_colorkey((0,0,0))
         
         self.show_targets = []
-        self.show_targets_poses = [[[-64,250],[275,250]],[[1088+self.brick.get_width(),250],[1088,250]]]
-        #self.show_targets.append(Showing_Target([-64,250],[275,250],self.showTarget,self.brick,1))
-        #self.show_targets.append(Showing_Target([1088+self.brick.get_width(),250],[1088,250],self.showTarget,self.brick,1))
+        self.show_targets_poses = [([-64,250],[275,250]),([1088+self.brick.get_width(),250],[1088,250])]
+
+        self.images = [
+            pygame.image.load('data/assets/metal96.png').convert(),
+            pygame.image.load('data/assets/regular96.png').convert(),
+            pygame.image.load('data/assets/dead96.png').convert()
+            ]
+
+        self.pieceImgs = [
+            pygame.image.load('data/assets/targetPiece1.png').convert(),
+            pygame.image.load('data/assets/targetPiece2.png').convert(),
+            pygame.image.load('data/assets/targetPiece3.png').convert(),
+        ]
+            
+        for img in self.images:
+            img.set_colorkey((0,0,0))
+
+        for img in self.pieceImgs:
+            img.set_colorkey((0,0,0))
+            
+        self.armored_targets = []
+
         self.appendUpdt = time.time()+1
 
         self.mp = [0,0]
@@ -164,28 +207,50 @@ class Target_System():
     def draw(self,screen):
         #Append targets
         if self.appendUpdt < time.time():
-            if rd.randint(0,5) == 5:
+            if rd.randint(0,10) == 10:
+                self.armored_targets.append(Armored_Target([rd.randint(0,1024),-64],self.images))
+            elif rd.randint(0,5) == 5:
                 pos = rd.choice(self.show_targets_poses)
                 ps = rd.randint(100,600)
                 pos[0][1] = ps
                 pos[1][1] = ps
                 self.show_targets.append(Showing_Target(pos[0],pos[1],self.showTarget,self.showTargetDestroyed,self.brick,1))
+            else:
+                self.targets.append(Target([rd.randint(0,1024),-64],self.target_1))
+                self.appendUpdt = time.time()+1
 
-            self.targets.append(Target([rd.randint(0,1024),-64],self.target_1))
-            self.appendUpdt = time.time()+1
-
+            
         #loop targets
         for target in self.targets:
             target.draw(screen)
+            
             if target.rect.top > 768:
-                self.targets.pop(self.targets.index(target))
+                try:
+                    self.targets.pop(self.targets.index(target))
+                except:
+                    pass
                 #self.targets.append(Target([rd.randint(0,1024),-64],self.target_1))
 
         for target in self.show_targets:
             target.draw(screen)
 
             if target.ease.percent <= 0.0:
-                self.show_targets.pop(self.show_targets.index(target))
+                try:
+                    self.show_targets.pop(self.show_targets.index(target))
+                except:
+                    pass
+                
+        for piece in self.pieces:
+            piece.draw(screen)
+            
+        for target in self.armored_targets:
+            target.draw(screen)
+            if target.rect.top > 768:
+                try:
+                    self.armored_targets.pop(self.targets.index(target))
+                except:
+                    pass
+                
     def handle_event(self,event):
         if event.type == MOUSEMOTION:
             self.mp = event.pos
@@ -196,7 +261,17 @@ class Target_System():
                 for target in self.targets:
                     if target.rect.colliderect(self.cursor):
                         self.targets.pop(self.targets.index(target))
-                        
+                        for x in range(3):
+                            self.pieces.append(target_piece([target.rect.center[0]+rd.randint(-30,30),target.rect.center[1]+rd.randint(-30,30)],self.pieceImgs[x]))
                 for target in self.show_targets:
                     if target.imgrect.colliderect(self.cursor):
                         target.destroyed = True
+
+                for target in self.armored_targets:
+                    if target.rect.colliderect(self.cursor):
+                        target.crntImg += 1
+
+                    if target.destroyed == True:
+                        self.armored_targets.pop(self.armored_targets.index(target))
+                        for x in range(3):
+                            self.pieces.append(target_piece([target.rect.center[0]+rd.randint(-30,30),target.rect.center[1]+rd.randint(-30,30)],self.pieceImgs[x]))
